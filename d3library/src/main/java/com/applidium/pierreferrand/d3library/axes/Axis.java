c57be45f86c82ef1a0cf4413beb5f39b3f890833
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-03-30 16:38:01 +0200

    Add Axis methods
    Change-Id: Ibf99cd52173197d3e1548b19e9689a891327852e

--- d3library/src/main/java/com/applidium/pierreferrand/d3library/axes/Axis.java
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/axes/Axis.java
@@ -2,6 +2,7 @@ package com.applidium.pierreferrand.d3library.axes;
 
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.support.annotation.ColorInt;
 
 import com.applidium.pierreferrand.d3library.scale.Scale;
 
@@ -40,5 +41,137 @@ public class Axis {
 
         textPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         textPaint.setStyle(Paint.Style.FILL);
+    }
+
+    /***
+     * Set the color of the stroke for the Axis and the ticks of this axis
+     */
+    public Axis axisColor(@ColorInt int color) {
+        paint.setColor(color);
+        return this;
+    }
+
+    /***
+     * Set the width of the stroke for the Axis and the ticks
+     */
+    public Axis axisWidth(float width) {
+        paint.setStrokeWidth(width);
+        return this;
+    }
+
+    /***
+     * Set the coordinates of the origin
+     */
+    public Axis translate(float offsetX, float offsetY) {
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        return this;
+    }
+
+    /***
+     * @return the number of ticks for the Axis
+     */
+    public int ticks() {
+        return ticksNumber;
+    }
+
+    /***
+     * Set the number of ticks for the Axis
+     */
+    public Axis ticks(int ticksNumber) {
+        ticks = null;
+        this.ticksNumber = ticksNumber;
+        return this;
+    }
+
+    /***
+     * Reset ticks : default Scale ticks will be used
+     */
+    public Axis tickValues() {
+        ticks = null;
+        ticksNumber = DEFAULT_TICK_NUMBER;
+        return this;
+    }
+
+    /***
+     * Set the the ticks to use rather than default Scale ticks
+     */
+    public Axis tickValues(float[] ticks) {
+        if (ticks == null || ticks.length < 2) {
+            throw new IllegalStateException("TickValue must have at least 2 values");
+        }
+        ticksNumber = ticks.length;
+        this.ticks = ticks.clone();
+        return this;
+    }
+
+    /***
+     * @return the scale of the Axis
+     */
+    public Scale scale() {
+        return this.scale;
+    }
+
+    /***
+     * Set the scale of the Axis
+     */
+    public Axis scale(Scale scale) {
+        this.scale = scale;
+        return this;
+    }
+
+    /***
+     * @return the inner ticks' size
+     */
+    public float tickSize() {
+        return tickSizeInner();
+    }
+
+    /***
+     * Set the inner and outer ticks' size
+     */
+    public Axis tickSize(float size) {
+        tickSizeInner(size);
+        tickSizeOuter(size);
+        return this;
+    }
+
+    /***
+     * @return the inner ticks' size
+     */
+    public float tickSizeInner() {
+        return innerTickSize;
+    }
+
+    /***
+     * Set the inner ticks' size
+     */
+    public Axis tickSizeInner(float size) {
+        innerTickSize = size;
+        return this;
+    }
+
+    /***
+     * @return the outer ticks' size
+     */
+    public float tickSizeOuter() {
+        return outerTickSize;
+    }
+
+    /***
+     * Set the outer ticks' size
+     */
+    public Axis tickSizeOuter(float size) {
+        outerTickSize = size;
+        return this;
+    }
+
+    private float firstBoundRange() {
+        return scale.range()[0];
+    }
+
+    private float lastBoundRange() {
+        float[] range = scale.range();
+        return range[range.length - 1];
     }
 }
