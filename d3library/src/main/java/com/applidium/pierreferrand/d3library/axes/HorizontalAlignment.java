e7d165ef37870cf7c16e98eed31be02519b61a32
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-03-30 17:09:08 +0200

    Add Alignement Enums
    Change-Id: I800193a7c77ac460dc2a7f415ef0ff796f32cbb2

---
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/axes/HorizontalAlignment.java
@@ -0,0 +1,16 @@
+package com.applidium.pierreferrand.d3library.axes;
+
+public enum HorizontalAlignment {
+    /***
+     * The legend is at the left of the tick.
+     */
+    LEFT,
+    /***
+     * The legend is center on the tick.
+     */
+    CENTER,
+    /***
+     * The legend is at the right of the tick (Default value).
+     */
+    RIGHT
+}
---
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/axes/VerticalAlignment.java
@@ -0,0 +1,16 @@
+package com.applidium.pierreferrand.d3library.axes;
+
+public enum VerticalAlignment {
+    /***
+     * The legend is above the tick (Default value).
+     */
+    TOP,
+    /***
+     * The legend is align with the tick.
+     */
+    CENTER,
+    /***
+     * The legend is under the tick.
+     */
+    BOTTOM
+}
