48a70101f0f2f5c07706e17c55c1f6dfe6c5de20
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-04-05 11:28:26 +0200

    Add onPinch in D3Drawable
    Change-Id: I14c727abdaa16367714b3efe06f448ef1dfe348f

--- d3library/src/main/java/com/applidium/pierreferrand/d3library/D3Drawable.java
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/D3Drawable.java
@@ -4,14 +4,18 @@ import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 
 import com.applidium.pierreferrand.d3library.action.OnClickAction;
+import com.applidium.pierreferrand.d3library.action.OnPinchAction;
 import com.applidium.pierreferrand.d3library.action.OnScrollAction;
+import com.applidium.pierreferrand.d3library.action.PinchType;
 import com.applidium.pierreferrand.d3library.action.ScrollDirection;
+
 public abstract class D3Drawable {
     private float height;
     private float width;
 
     @Nullable private OnClickAction onClickAction;
     @Nullable private OnScrollAction onScrollAction;
+    @Nullable private OnPinchAction onPinchAction;
 
     abstract public void draw(Canvas canvas);
 
@@ -49,4 +53,17 @@ public abstract class D3Drawable {
             onScrollAction.onScroll(direction, dX, dY);
         }
     }
+
+    public D3Drawable onPinchAction(OnPinchAction onPinchAction) {
+        this.onPinchAction = onPinchAction;
+        return this;
+    }
+
+    public void onPinch(PinchType pinchType, float dX, float dY) {
+        if (onPinchAction != null) {
+            onPinchAction.onPinch(pinchType, dX, dY);
+        }
+    }
+
+
 }
