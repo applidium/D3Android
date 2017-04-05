60a176e03c4f5549e6414c392b1553421770c6ef
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-04-05 11:26:05 +0200

    Add enum types for scroll and pinch
    Change-Id: Icda1c8e778b8b078c9b4ef8dff5f46803980338d

---
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/action/PinchType.java
@@ -0,0 +1,8 @@
+package com.applidium.pierreferrand.d3library.action;
+
+public enum PinchType {
+    VERTICAL_INCREASE,
+    VERTICAL_DECREASE,
+    HORIZONTAL_INCREASE,
+    HORIZONTAL_DECREASE
+}
---
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/action/ScrollDirection.java
@@ -0,0 +1,8 @@
+package com.applidium.pierreferrand.d3library.action;
+
+public enum ScrollDirection {
+    TOP,
+    RIGHT,
+    BOTTOM,
+    LEFT
+}
