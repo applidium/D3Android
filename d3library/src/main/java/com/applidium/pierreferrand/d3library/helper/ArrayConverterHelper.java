18ec13559313fd4bca3ad0065b69fe915cfe48c6
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-04-28 17:06:10 +0200

    Refactor ArrayConverterHelper
    Change-Id: I073d805d8f85b4bfd7563244e6d78e5b40f72d2b

--- d3library/src/main/java/com/applidium/pierreferrand/d3library/helper/ArrayConverterHelper.java
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/helper/ArrayConverterHelper.java
@@ -1,10 +1,12 @@
 package com.applidium.pierreferrand.d3library.helper;
 
-public class ArrayConverterHelper {
+public final class ArrayConverterHelper {
+    private ArrayConverterHelper(){}
+
     public static float[] convertArray(Float[] toConvert) {
         float[] result = new float[toConvert.length];
         for (int i = 0; i < result.length; i++) {
-            result[i] = toConvert[i] == null ? 0f : toConvert[i];
+            result[i] = toConvert[i] == null ? 0F : toConvert[i];
         }
         return result;
     }
