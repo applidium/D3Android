ea6e446ad0a1d2dbe75cf5c2020ad2d0ecc078b7
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-04-28 16:34:42 +0200

    Refactor D3Arc
    Change-Id: I65c7b395a511bb25af80584f3a242acbef3d3e39

--- d3library/src/main/java/com/applidium/pierreferrand/d3library/arc/D3Arc.java
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/arc/D3Arc.java
@@ -5,6 +5,8 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.applidium.pierreferrand.d3library.D3Drawable;
 import com.applidium.pierreferrand.d3library.Line.D3DataMapperFunction;
@@ -16,45 +18,51 @@ import com.applidium.pierreferrand.d3library.helper.ColorHelper;
 import com.applidium.pierreferrand.d3library.helper.TextHelper;
 
 public class D3Arc<T> extends D3Drawable {
-    private static float DEFAULT_LABEL_TEXT_SIZE = 25f;
-    private static float DEFAULT_PAD_ANGLE = 1f;
+    private static final float DEFAULT_LABEL_TEXT_SIZE = 25F;
+    private static final float DEFAULT_PAD_ANGLE = 1F;
+    private static final float HALF_CIRCLE_ANGLE = 180F;
+    private static final float CIRCLE_ANGLE = 360F;
+    private static final String INNER_RADIUS_ERROR = "InnerRadius should not be null.";
+    private static final String OUTER_RADIUS_ERROR = "OuterRadius should not be null.";
+    private static final String DATA_ERROR = "Data should not be null.";
 
-    private int[] colors = new int[]{0xFF0000FF, 0xFFFF0000, 0xFF00FF00, 0xFF000000};
+    @NonNull private int[] colors = new int[]{0xFF0000FF, 0xFFFF0000, 0xFF00FF00, 0xFF000000};
 
-    private D3FloatFunction outerRadius;
-    private D3FloatFunction innerRadius;
+    @Nullable private D3FloatFunction outerRadius;
+    @Nullable private D3FloatFunction innerRadius;
 
     private float padAngle;
 
-    private D3FloatFunction offsetX;
-    private D3FloatFunction offsetY;
+    @NonNull private D3FloatFunction offsetX;
+    @NonNull private D3FloatFunction offsetY;
 
-    private T[] data;
-    private D3DataMapperFunction<T> values;
-    private D3DataMapperFunction<T> weights;
+    @Nullable private T[] data;
+    @Nullable private D3DataMapperFunction<T> weights;
 
-    private String[] labels;
-    private Paint textPaint;
+    @Nullable private String[] labels;
+    @NonNull private Paint textPaint;
 
     public D3Arc() {
         this(null);
     }
 
-    public D3Arc(T[] data) {
-        data(data);
+    public D3Arc(@Nullable T[] data) {
+        if (data != null) {
+            data(data);
+        }
         weights(new D3DataMapperFunction<T>() {
             @Override public float compute(T object, int position, T[] data) {
-                return 1f;
+                return 1F;
             }
         });
-        innerRadius(0f);
+        innerRadius(0F);
         outerRadius(new D3FloatFunction() {
             @Override public float getFloat() {
                 return Math.min(height(), width()) / 2;
             }
         });
-        offsetX(0f);
-        offsetY(0f);
+        offsetX(0F);
+        offsetY(0F);
         padAngle(DEFAULT_PAD_ANGLE);
         setupPaint();
     }
@@ -66,10 +74,19 @@ public class D3Arc<T> extends D3Drawable {
         textPaint.setStyle(Paint.Style.FILL);
     }
 
+    /**
+     * Returns the inner radius of the Arc.
+     */
     public float innerRadius() {
+        if (innerRadius == null) {
+            throw new IllegalStateException(INNER_RADIUS_ERROR);
+        }
         return innerRadius.getFloat();
     }
 
+    /**
+     * Sets the inner radius of the Arc.
+     */
     public D3Arc<T> innerRadius(final float innerRadius) {
         this.innerRadius = new D3FloatFunction() {
             @Override public float getFloat() {
@@ -79,15 +96,27 @@ public class D3Arc<T> extends D3Drawable {
         return this;
     }
 
-    public D3Arc<T> innerRadius(D3FloatFunction innerRadius) {
+    /**
+     * Sets the inner radius of the Arc.
+     */
+    public D3Arc<T> innerRadius(@NonNull D3FloatFunction innerRadius) {
         this.innerRadius = innerRadius;
         return this;
     }
 
+    /**
+     * Returns the outer radius of the Arc.
+     */
     public float outerRadius() {
+        if (outerRadius == null) {
+            throw new IllegalStateException(OUTER_RADIUS_ERROR);
+        }
         return outerRadius.getFloat();
     }
 
+    /**
+     * Sets the outer radius of the Arc.
+     */
     public D3Arc<T> outerRadius(final float outerRadius) {
         this.outerRadius = new D3FloatFunction() {
             @Override public float getFloat() {
@@ -97,33 +126,56 @@ public class D3Arc<T> extends D3Drawable {
         return this;
     }
 
-    public D3Arc<T> outerRadius(D3FloatFunction outerRadius) {
+    /**
+     * Sets the outer radius of the Arc.
+     */
+    public D3Arc<T> outerRadius(@NonNull D3FloatFunction outerRadius) {
         this.outerRadius = outerRadius;
         return this;
     }
 
-    public T[] data() {
-        return data.clone();
+    /**
+     * Returns the data used by the Arc.
+     */
+    @Nullable public T[] data() {
+        return data != null ? data.clone() : null;
     }
 
-    public D3Arc<T> data(T[] data) {
+    /**
+     * Sets the data used by the Arc.
+     */
+    public D3Arc<T> data(@NonNull T[] data) {
         this.data = data.clone();
         return this;
     }
 
-    public int[] colors() {
+    /**
+     * Returns the colors used when the Arc is drawn.
+     */
+    @NonNull public int[] colors() {
         return colors;
     }
 
-    public D3Arc<T> colors(int[] colors) {
+    /**
+     * Sets the colors used when the Arc is drawn.
+     * The colors should have a 0xAARRGGBB format. If there are more data to display than colors,
+     * the colors will be used circularly.
+     */
+    public D3Arc<T> colors(@NonNull int[] colors) {
         this.colors = colors;
         return this;
     }
 
+    /**
+     * Returns the horizontal offset of the Arc.
+     */
     public float offsetX() {
         return offsetX.getFloat();
     }
 
+    /**
+     * Sets the horizontal offset of the Arc.
+     */
     public D3Arc<T> offsetX(final float offsetX) {
         this.offsetX = new D3FloatFunction() {
             @Override public float getFloat() {
@@ -133,15 +185,24 @@ public class D3Arc<T> extends D3Drawable {
         return this;
     }
 
-    public D3Arc<T> offsetX(D3FloatFunction offsetX) {
+    /**
+     * Sets the horizontal offset of the Arc.
+     */
+    public D3Arc<T> offsetX(@NonNull D3FloatFunction offsetX) {
         this.offsetX = offsetX;
         return this;
     }
 
+    /**
+     * Returns the vertical offset of the Arc.
+     */
     public float offsetY() {
         return offsetY.getFloat();
     }
 
+    /**
+     * Sets the vertical offset of the Arc.
+     */
     public D3Arc<T> offsetY(final float offsetY) {
         this.offsetY = new D3FloatFunction() {
             @Override public float getFloat() {
@@ -151,12 +212,24 @@ public class D3Arc<T> extends D3Drawable {
         return this;
     }
 
-    public D3Arc<T> offsetY(D3FloatFunction offsetY) {
+    /**
+     * Sets the vertical offset of the Arc.
+     */
+    public D3Arc<T> offsetY(@NonNull D3FloatFunction offsetY) {
         this.offsetY = offsetY;
         return this;
     }
 
-    public float[] weights() {
+    /**
+     * Returns the weight used to compute the proportion of each data.
+     */
+    @NonNull public float[] weights() {
+        if (data == null) {
+            throw new IllegalStateException(DATA_ERROR);
+        }
+        if (weights == null) {
+            return new float[0];
+        }
         float[] result = new float[data.length];
         for (int i = 0; i < data.length; i++) {
             result[i] = weights.compute(data[i], i, data);
@@ -164,7 +237,10 @@ public class D3Arc<T> extends D3Drawable {
         return result;
     }
 
-    public D3Arc<T> weights(final float[] weights) {
+    /**
+     * Sets the weight used to compute the proportion of each data.
+     */
+    public D3Arc<T> weights(@NonNull final float[] weights) {
         this.weights = new D3DataMapperFunction<T>() {
             private float[] customWeights = weights.clone();
 
@@ -175,28 +251,26 @@ public class D3Arc<T> extends D3Drawable {
         return this;
     }
 
-    public D3Arc<T> weights(D3DataMapperFunction<T> weights) {
+    /**
+     * Sets the weight used to compute the proportion of each data.
+     */
+    public D3Arc<T> weights(@NonNull D3DataMapperFunction<T> weights) {
         this.weights = weights;
         return this;
     }
 
-    public float[] values() {
-        float[] result = new float[data.length];
-        for (int i = 0; i < data.length; i++) {
-            result[i] = values.compute(data[i], i, data);
-        }
-        return result;
+    /**
+     * Sets the float values associated to each data.
+     */
+    @Nullable public String[] labels() {
+        return labels != null ? labels.clone() : null;
     }
 
-    public D3Arc<T> values(D3DataMapperFunction<T> values) {
-        this.values = values;
-        return this;
-    }
-
-    public String[] labels() {
-        return labels();
-    }
-
+    /**
+     * Defines if the labels must be drawn. If parameter is set to false, no label will be drawn.
+     * If parameter is set to true, the labels will be computed to the current data using toString()
+     * method on each data.
+     */
     public D3Arc<T> labels(boolean drawLabelsDependingOnData) {
         if (drawLabelsDependingOnData) {
             labels = new String[data.length];
@@ -209,103 +283,123 @@ public class D3Arc<T> extends D3Drawable {
         return this;
     }
 
-    public D3Arc<T> labels(String[] labels) {
+    /**
+     * Sets the labels to use.
+     */
+    public D3Arc<T> labels(@NonNull String[] labels) {
         this.labels = labels.clone();
         return this;
     }
 
+    /**
+     * Returns the pad angle between each part.
+     */
     public float padAngle() {
         return padAngle;
     }
 
+    /**
+     * Sets the pad angle between each part.
+     */
     public D3Arc<T> padAngle(float padAngle) {
         this.padAngle = padAngle;
         return this;
     }
 
-    public Object dataFromPosition(float x, float y) {
+    /**
+     * Returns the data corresponding to the part draw using the given coordinates.
+     * Return null if it does not match any part.
+     */
+    @Nullable public T dataFromPosition(float x, float y) {
         float xCenter = offsetX() + outerRadius();
         float yCenter = offsetY() + outerRadius();
 
-        float diffX = (x - xCenter);
-        float diffY = (y - yCenter);
+        float diffX = x - xCenter;
+        float diffY = y - yCenter;
 
         float radius = (float) Math.hypot(diffX, diffY);
-        if (radius < innerRadius() || radius > outerRadius()) {
+        if (data == null || radius < innerRadius() || radius > outerRadius()) {
             return null;
         }
 
         float angle = (float) Math.atan(diffY / diffX);
-        angle += angle < 0f ? Math.PI : 0f;
-        angle += diffY < 0f ? Math.PI : 0f;
-        angle = (float) (angle * 180f / Math.PI);
-        angle = 360f - angle;
+        angle += angle < 0F ? Math.PI : 0F;
+        angle += diffY < 0F ? Math.PI : 0F;
+        angle = (float) (angle * HALF_CIRCLE_ANGLE / Math.PI);
+        angle = CIRCLE_ANGLE - angle;
 
-        float[] weights = this.weights();
-        float totalWeight = 0f;
+        float[] computedWeights = this.weights();
+        float totalWeight = 0F;
 
         for (int i = 0; i < data.length; i++) {
-            totalWeight += weights[i];
+            totalWeight += computedWeights[i];
         }
-        if (totalWeight == 0f) {
+        if (totalWeight == 0F) {
             return null;
         }
 
-        float currentAngle = 0f;
+        float currentAngle = 0F;
         int indexData = -1;
         while (currentAngle < angle) {
             indexData++;
-            currentAngle += 360f * weights[indexData] / totalWeight;
+            currentAngle += CIRCLE_ANGLE * computedWeights[indexData] / totalWeight;
         }
         return data[indexData];
     }
 
-    @Override public D3Arc<T> onClickAction(OnClickAction onClickAction) {
+    @Override public D3Arc<T> onClickAction(@NonNull OnClickAction onClickAction) {
         super.onClickAction(onClickAction);
         return this;
     }
 
-    @Override public D3Arc<T> onScrollAction(OnScrollAction onScrollAction) {
+    @Override public D3Arc<T> onScrollAction(@NonNull OnScrollAction onScrollAction) {
         super.onScrollAction(onScrollAction);
         return this;
     }
 
-    @Override public D3Arc<T> onPinchAction(OnPinchAction onPinchAction) {
+    @Override public D3Arc<T> onPinchAction(@NonNull OnPinchAction onPinchAction) {
         super.onPinchAction(onPinchAction);
         return this;
     }
 
-    @Override public void draw(Canvas canvas) {
-        float[] weights = weights();
-        float totalWeight = 0f;
+    @Override public void draw(@NonNull Canvas canvas) {
+        if (data == null) {
+            throw new IllegalStateException(DATA_ERROR);
+        }
+        float[] computedWeights = weights();
+        float totalWeight = 0F;
 
         for (int i = 0; i < data.length; i++) {
-            totalWeight += weights[i];
+            totalWeight += computedWeights[i];
         }
-        if (totalWeight == 0f) {
+        if (totalWeight == 0F) {
             return;
         }
 
-        drawPie(canvas, weights, totalWeight);
-        drawLabels(canvas, weights, totalWeight);
+        drawPie(canvas, computedWeights, totalWeight);
+        drawLabels(canvas, computedWeights, totalWeight);
     }
 
-    private void drawPie(Canvas canvas, float[] weights, float totalWeight) {
-        float outerRadius = outerRadius();
-        float currentAngle = -padAngle / 2f;
+    private void drawPie(@NonNull Canvas canvas, @NonNull float[] weights, float totalWeight) {
+        if (data == null) {
+            throw new IllegalStateException(DATA_ERROR);
+        }
+        float computedOuterRadius = outerRadius();
+        float currentAngle = -padAngle / 2F;
 
         Bitmap bitmap = Bitmap.createBitmap(
-            (int) (2 * outerRadius), (int) (2 * outerRadius), Bitmap.Config.ARGB_8888
+            (int) (2 * computedOuterRadius),
+            (int) (2 * computedOuterRadius), Bitmap.Config.ARGB_8888
         );
         Canvas c = new Canvas(bitmap);
         for (int i = 0; i < data.length; i++) {
             paint.setColor(colors[i % colors.length]);
-            float drawAngle = (360f - weights.length * padAngle) * weights[i] / totalWeight;
+            float drawAngle = (CIRCLE_ANGLE - weights.length * padAngle) * weights[i] / totalWeight;
             c.drawArc(
-                0f,
-                0f,
-                2f * outerRadius,
-                2f * outerRadius,
+                0F,
+                0F,
+                2F * computedOuterRadius,
+                2F * computedOuterRadius,
                 currentAngle,
                 -drawAngle,
                 true,
@@ -314,31 +408,34 @@ public class D3Arc<T> extends D3Drawable {
             currentAngle -= drawAngle + padAngle;
         }
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
-        c.drawCircle(outerRadius, outerRadius, innerRadius(), paint);
+        c.drawCircle(computedOuterRadius, computedOuterRadius, innerRadius(), paint);
         paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC));
 
         canvas.drawBitmap(bitmap, offsetX(), offsetY(), null);
     }
 
 
-    private void drawLabels(Canvas canvas, float[] weights, float totalWeight) {
+    private void drawLabels(@NonNull Canvas canvas, @NonNull float[] weights, float totalWeight) {
         if (labels == null) {
             return;
         }
+        if (data == null) {
+            throw new IllegalStateException(DATA_ERROR);
+        }
 
-        float currentAngle = 0f;
-        float radius = (outerRadius() + innerRadius()) / 2f;
+        float currentAngle = 0F;
+        float radius = (outerRadius() + innerRadius()) / 2F;
         float realOffsetX = offsetX() + outerRadius();
         float realOffsetY = offsetY() + outerRadius();
 
         for (int i = 0; i < data.length; i++) {
             textPaint.setColor(ColorHelper.colorDependingOnBackground(colors[i % colors.length]));
-            float nextAngle = currentAngle + 360.0f * weights[i] / totalWeight;
-            float radianAngle = (float) Math.toRadians((nextAngle + currentAngle) / 2f);
+            float nextAngle = currentAngle + CIRCLE_ANGLE * weights[i] / totalWeight;
+            float radianAngle = (float) Math.toRadians((nextAngle + currentAngle) / 2F);
             float coordinateX = realOffsetX + radius * (float) Math.cos(radianAngle);
-            coordinateX -= textPaint.measureText(labels[i]) / 2f;
+            coordinateX -= textPaint.measureText(labels[i]) / 2F;
             float coordinateY = realOffsetY - radius * (float) Math.sin(radianAngle);
-            coordinateY += TextHelper.getTextHeight(labels[i], textPaint) / 2f;
+            coordinateY += TextHelper.getTextHeight(labels[i], textPaint) / 2F;
             canvas.drawText(labels[i], coordinateX, coordinateY, textPaint);
             currentAngle = nextAngle;
         }
