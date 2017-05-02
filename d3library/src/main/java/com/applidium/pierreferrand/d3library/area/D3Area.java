353de755f42ed568c2749c6e2b387ae4cb10c19c
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-05-02 16:42:24 +0200

    Refactor D3Area
    Change-Id: I0e5c14e8562ddc9ba910fe0750ce4faa07954a98

--- d3library/src/main/java/com/applidium/pierreferrand/d3library/area/D3Area.java
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/area/D3Area.java
@@ -3,6 +3,8 @@ package com.applidium.pierreferrand.d3library.area;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 import com.applidium.pierreferrand.d3library.Line.D3DataMapperFunction;
 import com.applidium.pierreferrand.d3library.Line.D3Line;
@@ -13,69 +15,86 @@ import com.applidium.pierreferrand.d3library.axes.D3FloatFunction;
 import com.applidium.pierreferrand.d3library.scale.Interpolator;
 
 public class D3Area<T> extends D3Line<T> {
-    private D3FloatFunction ground;
+    private static final String GROUND_ERROR = "Ground should not be null";
+    private static final String DATA_ERROR = "Data should not be null";
+    @Nullable private D3FloatFunction ground;
 
     public D3Area() {
         super();
     }
 
-    public D3Area(T[] data) {
+    public D3Area(@Nullable T[] data) {
         super(data);
         setupPaint();
     }
 
-    @Override public D3Area<T> onClickAction(OnClickAction onClickAction) {
+    @Override public D3Area<T> onClickAction(@NonNull OnClickAction onClickAction) {
         super.onClickAction(onClickAction);
         return this;
     }
 
-    @Override public D3Area<T> onScrollAction(OnScrollAction onScrollAction) {
+    @Override public D3Area<T> onScrollAction(@NonNull OnScrollAction onScrollAction) {
         super.onScrollAction(onScrollAction);
         return this;
     }
 
-    @Override public D3Area<T> onPinchAction(OnPinchAction onPinchAction) {
+    @Override public D3Area<T> onPinchAction(@NonNull OnPinchAction onPinchAction) {
         super.onPinchAction(onPinchAction);
         return this;
     }
 
-    @Override public D3Area<T> x(D3DataMapperFunction<T> function) {
-        super.x(function);
+    @Override public D3Area<T> x(@NonNull D3DataMapperFunction<T> x) {
+        super.x(x);
         return this;
     }
 
-    @Override public D3Area<T> y(D3DataMapperFunction<T> function) {
-        super.y(function);
+    @Override public D3Area<T> y(@NonNull D3DataMapperFunction<T> y) {
+        super.y(y);
         return this;
     }
 
-    @Override public D3Area<T> data(T[] data) {
+    @Override public D3Area<T> data(@NonNull T[] data) {
         super.data(data);
         return this;
     }
 
-    @Override public D3Area<T> interpolator(Interpolator interpolator) {
+    @Override public D3Area<T> interpolator(@NonNull Interpolator interpolator) {
         super.interpolator(interpolator);
         return this;
     }
 
-    @Override public D3Area<T> paint(Paint paint) {
+    @Override public D3Area<T> paint(@NonNull Paint paint) {
         super.paint(paint);
         return this;
     }
 
+    /**
+     * Returns the ground of the area.
+     */
     public float ground() {
+        if (ground == null) {
+            throw new IllegalStateException(GROUND_ERROR);
+        }
         return ground.getFloat();
     }
 
-    public D3Area<T> ground(D3FloatFunction ground) {
+    /**
+     * Sets the ground of the area.
+     */
+    public D3Area<T> ground(@NonNull D3FloatFunction ground) {
         this.ground = ground;
         return this;
     }
 
-    @Override public void draw(Canvas canvas) {
+    @Override public void draw(@NonNull Canvas canvas) {
+        if (data == null) {
+            throw new IllegalStateException(DATA_ERROR);
+        }
         if (data.length < 2) {
             return;
+        }
+        if (ground == null) {
+            throw new IllegalStateException(GROUND_ERROR);
         }
 
         float[] x = x();
