5a2a2a322231047e687cddca9e4dcce3ad3d2bbe
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-05-03 11:59:09 +0200

    Refactor D3Drawable
    Change-Id: I8b984814f89b9d1d9b3ca05acca4fc07e3f5fb78

--- d3library/src/main/java/com/applidium/pierreferrand/d3library/D3Drawable.java
+++ d3library/src/main/java/com/applidium/pierreferrand/d3library/D3Drawable.java
@@ -23,6 +23,10 @@ public abstract class D3Drawable {
     @Nullable private OnPinchAction onPinchAction;
     @NonNull protected Paint paint;
 
+    /**
+     * This method must be called in the constructor of each D3Drawable in order to setup
+     * the Paint
+     */
     protected void setupPaint() {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         paint.setColor(Color.rgb(0, 0, 0));
@@ -30,47 +34,70 @@ public abstract class D3Drawable {
         paint.setStrokeWidth(DEFAULT_STROKE_WIDTH);
     }
 
-    public final Paint paint() {
+    /**
+     * Returns the paint used for drawing
+     */
+    @NonNull public final Paint paint() {
         return paint;
     }
 
-    public D3Drawable paint(Paint paint) {
+    /**
+     * Sets the paint used for drawing
+     */
+    public D3Drawable paint(@NonNull Paint paint) {
         this.paint = paint;
         return this;
     }
 
-    abstract public void draw(Canvas canvas);
+    /**
+     * This inner method must not be called.
+     */
+    public abstract void draw(@NonNull Canvas canvas);
 
-    protected void setDimensions(float height, float width) {
+    final void setDimensions(float height, float width) {
         this.height = height;
         this.width = width;
     }
 
+    /**
+     * Returns the height available for the Drawable. It is correctly set just before the
+     * drawing.
+     */
     public float height() {
         return height;
     }
 
+    /**
+     * Returns the width available for the Drawable. It is correctly set just before the
+     * drawing.
+     */
     public float width() {
         return width;
     }
 
-    public D3Drawable onClickAction(OnClickAction onClickAction) {
+    /**
+     * Sets the action to execute when a click occurs.
+     */
+    public D3Drawable onClickAction(@Nullable OnClickAction onClickAction) {
         this.onClickAction = onClickAction;
         return this;
     }
 
-    public void onClick(float X, float Y) {
+    final void onClick(float x, float y) {
         if (onClickAction != null) {
-            onClickAction.onClick(X, Y);
+            onClickAction.onClick(x, y);
         }
     }
 
-    public D3Drawable onScrollAction(OnScrollAction onScrollAction) {
+    /**
+     * Sets the action to execute when a scroll occurs.
+     */
+    public D3Drawable onScrollAction(@Nullable OnScrollAction onScrollAction) {
         this.onScrollAction = onScrollAction;
         return this;
     }
 
-    public void onScroll(
+    final void onScroll(
         ScrollDirection direction, float coordinateX, float coordinateY, float dX, float dY
     ) {
         if (onScrollAction != null) {
@@ -78,12 +105,15 @@ public abstract class D3Drawable {
         }
     }
 
-    public D3Drawable onPinchAction(OnPinchAction onPinchAction) {
+    /**
+     * Sets the action to execute when a pinch occurs.
+     */
+    public D3Drawable onPinchAction(@Nullable OnPinchAction onPinchAction) {
         this.onPinchAction = onPinchAction;
         return this;
     }
 
-    public void onPinch(
+    final void onPinch(
         PinchType pinchType, float coordinateStaticX, float coordinateStaticY,
         float coordinateMobileX, float coordinateMobileY, float dX, float dY
     ) {
@@ -95,6 +125,9 @@ public abstract class D3Drawable {
         }
     }
 
+    /**
+     * This inner method must not be called.
+     */
     public void prepareParameters() {
         /* Nothing to do. Child classes can override this method to launch computation
          * of parameters before drawing. */
