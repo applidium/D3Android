f965ec6c2ba06caa0b1af4cf797fe296d226ac7c
Pierre Ferrand <pierre.ferrand@applidium.com>
2017-07-26 12:18:56 +0200

    Update test Activity
    Change-Id: I52e9726c258b1d289a7fd1d43f10e5cf8317202a

--- app/src/main/java/com/applidium/pierreferrand/d3_android/MainActivity.java
+++ app/src/main/java/com/applidium/pierreferrand/d3_android/MainActivity.java
@@ -8,28 +8,22 @@ import android.support.annotation.Nullable;
 
 import com.applidium.pierreferrand.d3library.D3Drawable;
 import com.applidium.pierreferrand.d3library.D3View;
-import com.applidium.pierreferrand.d3library.line.D3DataMapperFunction;
-import com.applidium.pierreferrand.d3library.action.OnPinchAction;
-import com.applidium.pierreferrand.d3library.action.OnScrollAction;
-import com.applidium.pierreferrand.d3library.action.PinchType;
-import com.applidium.pierreferrand.d3library.action.ScrollDirection;
+import com.applidium.pierreferrand.d3library.action.OnClickAction;
 import com.applidium.pierreferrand.d3library.axes.AxisOrientation;
 import com.applidium.pierreferrand.d3library.axes.D3Axis;
 import com.applidium.pierreferrand.d3library.axes.D3FloatFunction;
 import com.applidium.pierreferrand.d3library.axes.D3RangeFunction;
-import com.applidium.pierreferrand.d3library.barchart.D3StackBarChart;
+import com.applidium.pierreferrand.d3library.boxplot.D3BoxPlot;
+import com.applidium.pierreferrand.d3library.line.D3DataMapperFunction;
 import com.applidium.pierreferrand.d3library.scale.D3Converter;
 
 import net.danlew.android.joda.JodaTimeAndroid;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends Activity {
 
     D3View view;
 
-    D3Axis<Integer> numberAxis;
+    D3Axis<Float> numberAxis;
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,22 +34,22 @@ public class MainActivity extends Activity {
         view = (D3View) findViewById(R.id.test);
 
         numberAxis =
-            new D3Axis<Integer>(AxisOrientation.RIGHT)
+            new D3Axis<Float>(AxisOrientation.RIGHT)
                 .offsetX(new D3FloatFunction() {
                     @Override public float getFloat() {
                         return view.getWidth() * 0.05f;
                     }
                 })
-                .converter(new D3Converter<Integer>() {
-                    @Override public float convert(Integer toConvert) {
+                .converter(new D3Converter<Float>() {
+                    @Override public float convert(Float toConvert) {
                         return toConvert;
                     }
 
-                    @Override public Integer invert(float toInvert) {
-                        return (int) toInvert;
+                    @Override public Float invert(float toInvert) {
+                        return toInvert;
                     }
                 })
-                .domain(new Integer[]{0, 8000})
+                .domain(new Float[]{0f, 100f})
                 .range(new D3RangeFunction() {
                     float[] range = new float[2];
 
@@ -66,129 +60,31 @@ public class MainActivity extends Activity {
                     }
                 });
 
-        final D3Axis<Integer> saleAxis =
-            new D3Axis<Integer>(AxisOrientation.TOP)
-                .domain(new Integer[]{0, 5})
-                .ticks(6)
-                .range(new D3RangeFunction() {
-                    float[] range = new float[2];
+        Float[] data = new Float[50];
+        rerollData(data);
 
-                    @Override public float[] getRange() {
-                        range[0] = 0.05f * view.getWidth();
-                        range[1] = 0.95f * view.getWidth();
-                        return range;
-                    }
-                }).converter(new D3Converter<Integer>() {
-                @Override public float convert(Integer toConvert) {
-                    return toConvert;
-                }
-
-                @Override public Integer invert(float toInvert) {
-                    return (int) toInvert;
-                }
-            }).offsetY(new D3FloatFunction() {
+        final D3BoxPlot<Float> boxPlot = new D3BoxPlot<>(data)
+            .scale(numberAxis.scale())
+            .dataWidth(50f)
+            .offsetX(new D3FloatFunction() {
                 @Override public float getFloat() {
-                    return (float) (view.getHeight() * 0.98);
+                    return view.getWidth() / 2f;
                 }
             })
-                .onScrollAction(new OnScrollAction() {
-                    @Override public void onScroll(
-                        ScrollDirection direction,
-                        float coordinateX,
-                        float coordinateY,
-                        float dX,
-                        float dY
-                    ) {
-                    }
-                })
-                .onPinchAction(new OnPinchAction() {
-                    @Override public void onPinch(
-                        PinchType pinchType,
-                        float coordinateStaticX,
-                        float coordinateStaticY,
-                        float coordinateMobileX,
-                        float coordinateMobileY,
-                        float dX,
-                        float dY
-                    ) {
-                    }
-                });
+            .setDataMapper(new D3DataMapperFunction<Float>() {
+                @Override public float compute(Float object, int position, Float[] data) {
+                    return object;
+                }
+            });
+        boxPlot.onClickAction(new OnClickAction() {
+            @Override public void onClick(float X, float Y) {
+                boxPlot.updateNeeded();
+                rerollData(boxPlot.data());
+            }
+        }).lazyRecomputing(true);
 
-        Sales[] sales = new Sales[]{
-            new Sales(1, 3840, 1920, 960, 400),
-            new Sales(2, 1600, 1440, 960, 400),
-            new Sales(3, 640, 960, 940, 400),
-            new Sales(4, 320, 480, 640, 400)
-        };
-
-        D3StackBarChart<Sales> stackBarChart =
-            new D3StackBarChart<>(sales, 4)
-                .x(new D3DataMapperFunction<Sales>() {
-                    @Override public float compute(
-                        Sales object, int position, Sales[] data
-                    ) {
-                        return saleAxis.scale().value(object.sale);
-                    }
-                })
-                .y(new D3DataMapperFunction<Sales>() {
-                    @Override public float compute(
-                        Sales object, int position, Sales[] data
-                    ) {
-                        return numberAxis.scale().value(0);
-                    }
-                })
-                .dataWidth(100f)
-                .colors(new int[][]{
-                    new int[]{0xFF0000FF},
-                    new int[]{0xFF00FF00},
-                    new int[]{0xFF0000FF},
-                    new int[]{0xFFFF0000},
-                    });
-        List<D3DataMapperFunction<Sales>> heights = new ArrayList<>();
-        heights.add(
-            new D3DataMapperFunction<Sales>() {
-                @Override public float compute(
-                    Sales object, int position, Sales[] data
-                ) {
-                    return numberAxis.scale().value(0) - numberAxis
-                        .scale()
-                        .value(object.apples);
-                }
-            });
-        heights.add(
-            new D3DataMapperFunction<Sales>() {
-                @Override public float compute(
-                    Sales object, int position, Sales[] data
-                ) {
-                    return numberAxis.scale().value(0) - numberAxis
-                        .scale()
-                        .value(object.bananas);
-                }
-            });
-        heights.add(
-            new D3DataMapperFunction<Sales>() {
-                @Override public float compute(
-                    Sales object, int position, Sales[] data
-                ) {
-                    return numberAxis.scale().value(0) - numberAxis
-                        .scale()
-                        .value(object.cherries);
-                }
-            });
-        heights.add(
-            new D3DataMapperFunction<Sales>() {
-                @Override public float compute(
-                    Sales object, int position, Sales[] data
-                ) {
-                    return numberAxis.scale().value(0) - numberAxis
-                        .scale()
-                        .value(object.dates);
-                }
-            });
-        stackBarChart.dataHeight(heights);
         view.add(numberAxis);
-        view.add(saleAxis);
-        view.add(stackBarChart);
+        view.add(boxPlot);
         view.add(new D3Drawable() {
             @Override public void draw(@NonNull Canvas canvas) {
 
@@ -196,19 +92,9 @@ public class MainActivity extends Activity {
         }.lazyRecomputing(false));
     }
 
-    class Sales {
-        int sale;
-        int apples;
-        int bananas;
-        int cherries;
-        int dates;
-
-        private Sales(int sale, int apples, int bananas, int cherries, int dates) {
-            this.sale = sale;
-            this.apples = apples;
-            this.bananas = bananas;
-            this.cherries = cherries;
-            this.dates = dates;
+    private void rerollData(@NonNull Float[] data) {
+        for (int i = 0; i < data.length; i++) {
+            data[i] = (float) (Math.random() * 100);
         }
     }
 }
